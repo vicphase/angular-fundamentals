b51f3b6e39669eb5fd7064fbe07f74ba65593450
Víctor Martínez <victor.martinez@gdx.io>
2019-02-27 18:22:18 -0600

    feat(app): Add modules of our application

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  // Form controls
+  MatFormFieldModule,
+  MatInputModule,
+  // Navigation
+  MatSidenavModule,
+  MatToolbarModule,
+  // Layout
+  MatCardModule,
+  MatListModule,
+  // Buttons & Indicators
+  MatButtonModule,
+  MatIconModule,
+  MatProgressSpinnerModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, modules],
+  exports: [modules]
+})
+export class SharedModule {}
---
+++ src/app/spa/spa.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpaRoutingModule } from './spa-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpaRoutingModule
+  ]
+})
+export class SpaModule { }
