80e4768b20e1f1ca6eac6cdaeb4c39e7f5c3c820
Víctor Martínez <victor.martinez@gdx.io>
2019-03-03 13:00:33 -0600

    refactor(app): Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-essentials'`, () => {
+  it(`should have as title 'angular-fundamentals'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-essentials');
+    expect(app.title).toEqual('angular-fundamentals');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-essentials!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-fundamentals!');
   });
 });
